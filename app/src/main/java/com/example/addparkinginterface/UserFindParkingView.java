53786a28622bfe2d311d7e7eb229287a0f31ccc0
sarra halou <halousarra@gmail.com>
2023-06-06 15:27:10 +0100

    init7

---
+++ app/src/main/java/com/example/addparkinginterface/UserFindParkingView.java
@@ -0,0 +1,14 @@
+package com.example.addparkinginterface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserFindParkingView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_find_parking_view);
+    }
+}
